39455a3df7a1847d260b09c37e8dc5785bcf9b70
priyadharshini10122002 <priyadharshini10122002@gmail.com>
2024-09-29 12:18:57 +0530

    Login Process Completed

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer,LoginSerializer
 
 
 class RegisterView(APIView):
@@ -13,17 +13,45 @@ class RegisterView(APIView):
             data=request.data
             serializer=RegisterSerializer(data=data)
             if not serializer.is_valid():
+                print("Hey")
                 return Response({'data':serializer.errors,
                                 'message':'Something went wrong!'},
                                 status=status.HTTP_400_BAD_REQUEST)
         
             serializer.save()
 
-            return Response({'data':{},
+            return Response({'data':{serializer.data},
                              'message':'Your Account has Created Successfully!'},
                              status=status.HTTP_201_CREATED)
         
         except Exception as e:
+            print(e)
             return Response({'data':{},
                             'message':'Something went wrong!'},
                             status=status.HTTP_400_BAD_REQUEST)
+        
+
+class LoginView(APIView):
+    def post(self, request):
+        try:
+            data=request.data
+            serializer=LoginSerializer(data=data)
+
+            if not serializer.is_valid():
+                print("hey")
+                return Response({'data':serializer.errors,
+                                'message':'Something went wrong!'},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            response =serializer.get_jwt_token(serializer.data)
+
+            return Response(response,status=status.HTTP_200_OK)
+        
+        
+                
+        except Exception as e:
+            print(e)
+            return Response({'data':{},
+                            'message':'Something went wrong!'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        
